4ffd1c290b0bfbf3fbea81f697b7e4c4cff5b3bd
highlight1004 <1362287647@qq.com>
2017-10-09 18:49:26 +0800

    Update main.py

--- hw02/main.py
+++ hw02/main.py
@@ -1,20 +1,20 @@
-def BinSearch(list,value):
+def search(list,target):
     begin = 0
-    if list[0] == value:
+    if list[0] == target:
         return 0
 
     end = len(list)-1
     while(begin < end):
         mid = begin + (end - begin)//2
-        if list[mid]>value:
+        if list[mid]>target:
             end = mid - 1
-        elif list[mid] <value:
+        elif list[mid] <target:
             begin = mid + 1
         else:
             return mid
 
     if(begin == end):
-        if(value == list[begin]):
+        if(target == list[begin]):
             return begin
         else:
             return -1
